e2a8840e3fcec70ea4b2d10601f9f5bf7361976b
Dharani9018 <anu293575@gmail.com>
2025-11-23 16:52:40 +0530

    .

--- sem3/DSA/Trees/Heap_tree/BottomUp.c
+++ sem3/DSA/Trees/Heap_tree/BottomUp.c
@@ -4,8 +4,33 @@
 void bottom_up_heap(int *h,int n)
 {
     int i, j, k ,key;
-    
+
+    for( k = (n-1)/2;k>=0;k--)
+    {
+        j = k;
+        key = h[j];
+        i = 2*j + 1;
+        while(i<=n)
+        {
+            if(i + 1<=n && h[i+1] > h[i])
+            {
+                i++;
+            }
+            if(key<h[i])
+            {
+                h[j] = h[i];
+                j = i;
+                i = 2*j+1;
+            }
+            else
+            {
+                break;
+            }
+        }
+        h[j] = key;
+    }    
 }
+
 int main()
 {
     int h[100];
@@ -19,8 +44,13 @@ int main()
     {
         scanf("%d",h+i);
     }
+    printf("\nBefore:\n");
+    for(int i = 0;i<n;i++)
+    {
+        printf("%d\t",h[i]);
+    }
     bottom_up_heap(h,n-1);
-    printf("After bottom up heap construction: \n");
+    printf("\nAfter bottom up heap construction: \n");
     for(int i = 0;i<n;i++)
     {
         printf("%d\t",h[i]);
