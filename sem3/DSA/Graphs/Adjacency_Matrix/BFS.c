f46d1c44225f4e535cd8f4218dc85f28d6cd2c4e
Dharani9018 <anu293575@gmail.com>
2025-12-11 15:24:54 +0530

    hehehehe

--- sem3/DSA/Graphs/Adjacency_Matrix/BFS.c
+++ sem3/DSA/Graphs/Adjacency_Matrix/BFS.c
@@ -42,19 +42,20 @@ int *bfs(int v)
     int *visited = calloc(MAX, sizeof(int));
 
     visited[v] = 1;
-    queue[++r] = v;
+    queue[++r] = v; //insert starting element into queue
     printf("\nBFS traversal:\n");
     printf("%d\t", v);
 
-    while (f <= r)
+    while (f <= r) //loop until front less than or equal to rear
     {
-        v = queue[f++];
+        v = queue[f++]; //delete from front and store it in v (v->row index)
 
         for (int u = 0; u < n; u++)
         {
-            if (a[v][u] == 1 && visited[u] == 0)
+            if (a[v][u] == 1 && visited[u] == 0) //find neighbours of v and check if they're visited
             {
-                queue[++r] = u;
+                //if not visited, insert to queue, mark visited and print.
+                queue[++r] = u; 
                 visited[u] = 1;
                 printf("%d\t", u);
             }
@@ -70,11 +71,11 @@ void is_connected()
     f = 0;
     r = -1;
 
-    int *visited = bfs(v);
+    int *visited = bfs(v); //track visited array from bfs traversal.
 
     for (int i = 0; i < n; i++)
     {
-        if (!visited[i])
+        if (!visited[i]) //if any 1 of the vertex is not visited, then the graph is not connected.
         {
             printf("Not Connected\n");
             return;
@@ -93,24 +94,28 @@ void CheckCycle()
     f = 0;
     r = -1;
 
-    queue[++r] = 0;
-    visited[0] = 1;
+    queue[++r] = 0; //start from the vertex 0, insert it queue.
+    visited[0] = 1; //mark visited.
 
-    while (f <= r)
+    while (f <= r) //loop until from less than or equal to rear.
     {
-        int v = queue[f++];
+        int v = queue[f++]; //delete front and store it in v.
 
-        for (int u = 0; u < n; u++)
+        for (int u = 0; u < n; u++) 
         {
-            if (a[v][u] == 1)
+            if (a[v][u] == 1) //if u find a neighbour.
             {
-                if (!visited[u])
+                if (!visited[u]) //if the neighbour is not visitedd, mark visited.
                 {
                     visited[u] = 1;
-                    parent[u] = v;
-                    queue[++r] = u;
+                    parent[u] = v; //store the parent of u.
+                    queue[++r] = u; //insert to queue.
                 }
-                else if (u != parent[v])
+                else if (u != parent[v]) 
+                //if its already  visited and if u is not the parent of v
+                //, cycle detected!!  
+                //avoids detecting this as a cycle:
+                // [0] --- [1]      
                 {
                     printf("Cycle detected\n");
                     return;
@@ -130,43 +135,48 @@ void Bfspath(int s,int d)
     {
         parent[i] = -1;
     }
-    queue[++r] = s;
-    visited[s] = 1;
+    queue[++r] = s; //insert source to queue
+    visited[s] = 1;//mark visited.
     while(f<=r)
     {
-        int u = queue[f++];
-        if(u==d)
+        int u = queue[f++]; //delete from front and store it in u.
+        if(u==d) 
         {
             break;
         }
         for(int v=0;v<n;v++)
         {
-            if(a[u][v]==1 && !visited[v])
+            if(a[u][v]==1 && !visited[v]) //check if the neighbours are not visited.
             {
-                queue[++r] = v;
-                parent[v] = u;
-                visited[v] = 1;
+                queue[++r] = v; //insert to queue
+                parent[v] = u; //track parent(row)
+                visited[v] = 1;//mark visited.
             }
         }
     }
 }
 
+
 void printpath(int s,int d)
 {
+    //BFS used to find shortest path.
     Bfspath(s,d);
-    if(parent[d]==-1 && s!=d)
+    if(parent[d]==-1 && s!=d) //if d doesnt have a parent(means d is not a neighbour of any vertex) and source and destination are not the same.
     {
         printf("\nPath not found\n");
         return;
     }
     p = 0;
-    for(int v = d;v!=-1;v = parent[v])
+    for(int v = d;v!=-1;v = parent[v]) 
+    //ex d=3, parent[-1,0,0,1,3] after loop-> path[3,1,0]    
     {
         path[p++] = v;
     }
+    
     printf("\nPath from %d to %d\n",s,d);
     for(int i = p-1;i>=0;i--)
     {
+        //print path from reverse.
         printf("%d\t",path[i]);
     }
     printf("\n");
